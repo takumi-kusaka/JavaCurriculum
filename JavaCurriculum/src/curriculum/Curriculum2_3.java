8f2691d60fa4a029aa644b2fb078d508276ee6c9
k-taku3 <k-taku3@localhost>
2026-01-18 23:00:45 +0900

    カリキュラム2-3の提出課題です。

---
+++ JavaCurriculum/src/curriculum/Curriculum2_3.java
@@ -0,0 +1,19 @@
+package curriculum;
+
+public class Curriculum2_3 {
+	public static void main(String[] args) { 
+	// 氏名
+	String name = "山田　花子";
+	// 年齢
+	int age = 25;
+	// 趣味
+	String hobby = "映画鑑賞";
+	
+	/*
+	自己紹介文が出力する処理
+	*/
+	System.out.println("私の名前は" + name + "です。");
+	System.out.println("年齢は" + age + "歳です。");
+	System.out.println("趣味は" + hobby + "です。");
+	}
+}
