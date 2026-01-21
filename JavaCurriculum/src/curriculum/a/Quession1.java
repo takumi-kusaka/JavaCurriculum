773cf48d114335ef61e600d3041edd0be128f4b4
k-taku3 <k-taku3@localhost>
2026-01-21 23:34:39 +0900

    カリキュラム2-7、2-10の提出課題です。よろしくお願いします。

---
+++ JavaCurriculum/src/curriculum/a/Quession1.java
@@ -0,0 +1,89 @@
+package curriculum.a;
+
+public class Quession1 {
+	
+	public static void main(String[] args) {
+		
+		// Q1,Q2
+		byte byteNum = 10;
+		short shortNum = 100;
+		int intNum = 1000;
+		long longNum = 10000;
+		float floatNum = 9.5f;
+		double doubleNum = 10.5;
+		char letter = 'a';
+		String letters = "ハロー";
+		boolean isBoolean = true;
+
+		// Q3
+		System.out.println(byteNum);
+		System.out.println(shortNum / 5);
+		System.out.println(letter + letters + isBoolean);
+		System.out.println(byteNum + shortNum + intNum + longNum + floatNum + doubleNum);
+		System.out.println(byteNum * shortNum * intNum * longNum);
+		System.out.println(doubleNum / 100);
+		System.out.println(byteNum - shortNum);
+		System.out.println();
+
+		// Q4
+		String name = "山田太郎";
+		System.out.println("こんにちは、" + name + "さん！");
+		System.out.println();
+
+		// Q5
+		int age = 25;
+		System.out.println("年齢:" + age + "歳");
+		System.out.println();
+
+		// Q6
+		int num1 = 10;
+		int num2 = 5;
+		int sum = num1 + num2;
+		System.out.println(sum);
+		System.out.println();
+
+		// Q7
+		int score = 80;
+		score += 20;
+		System.out.println("最終スコア:" + score);
+		System.out.println();
+
+		// Q8
+		double price = 99.99;
+		// double型からint型への変換
+		System.out.println("整数価格:" + (int) price);
+		System.out.println();
+
+		// Q9
+		String numSt = "123";
+		// String型からint型への変換
+		System.out.println(Integer.parseInt(numSt) + 10);
+		System.out.println();
+
+		// Q10
+		int num = 50;
+		// int型からString型への変換
+		System.out.println("得点:" + String.valueOf(num));
+		System.out.println();
+
+		// Q11
+		int a = 10;
+		int b = 20;
+		// aの方が小さければtrue
+		boolean result = a < b;
+		System.out.println(result);
+		System.out.println();
+
+		// Q12
+		int x = 15;
+		System.out.println(x >= 10 ? "OK" : "NG");
+		System.out.println();
+
+		// Q13
+		String text = "私はJavaが好きです。Javaは楽しい!";
+		System.out.println(text.replace("Java", "Python"));
+		System.out.println();
+
+	}
+
+}
