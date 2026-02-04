8413a756169eba5efddc0152e6e67f80b7a1da6b
k-taku3 <k-taku3@mac>
2026-02-04 16:29:38 +0900

    オブジェクト指向まとめ ②パッケージf よろしくお願いします

---
+++ JavaCurriculum/src/curriculum/f/Employee.java
@@ -0,0 +1,14 @@
+package curriculum.f;
+
+public class Employee {
+
+	// フィールドを生成
+	String employeeId;
+	String name;
+
+	// メソッドを生成
+	public void showInfo() {
+		System.out.println("社員ID:" + employeeId + "、名前:" + name);
+	}
+
+}
