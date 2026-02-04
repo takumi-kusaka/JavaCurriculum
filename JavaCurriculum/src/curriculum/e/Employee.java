0dd6df315c07dc3de86210564040a075b28a6298
k-taku3 <k-taku3@mac>
2026-02-04 13:59:04 +0900

    オブジェクト指向まとめ ①のパッケージeです。 よろしくお願いします。

---
+++ JavaCurriculum/src/curriculum/e/Employee.java
@@ -0,0 +1,12 @@
+package curriculum.e;
+
+public class Employee {
+	// フィールドを生成
+	String name;
+
+	 // workメソッドを生成
+	public void work() {
+		System.out.println(name + "は働いています。");
+	}
+
+}
---
+++ JavaCurriculum/src/curriculum/e/Main.java
@@ -0,0 +1,12 @@
+package curriculum.e;
+
+public abstract class Main {
+	public static void main (String[] args) {
+
+		//  インスタンスを生成
+		Employee employee = new Employee();
+		employee.name = "山田太郎";
+		employee.work();
+
+	}
+}
